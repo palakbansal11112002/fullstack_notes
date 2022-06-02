cbc3428112018c63e2dc44cf7b8f34bb3d80595f
palakbansal11112002 <60420767+palakbansal11112002@users.noreply.github.com>
2022-06-02 23:34:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var myApp= angular.module("Module",[]);
+myApp.controller("Controller",function($scope)
+{
+$scope.name="SecA FS";
+$scope.seca_detail={name:'A',age:2022,mb:1111,gen:'M',email:'a@a.a'};
+}
+);
